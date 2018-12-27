823754cfb1d4963f097d3f4dae7f814f42ba7847
Rejfi <rrafaul@wp.pl>
2018-12-27 18:17:02 +0100

    Code review

--- app/src/main/java/com/revolshen/fragmentapp/DrawViewKT.kt
+++ app/src/main/java/com/revolshen/fragmentapp/DrawViewKT.kt
@@ -3,10 +3,8 @@ package com.revolshen.fragmentapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class DrawViewKT @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null): View(context, attributeSet) {
 
@@ -26,12 +24,12 @@ class DrawViewKT @JvmOverloads constructor(context: Context, attributeSet: Attri
                     circle = Circle()
                     circles.add(circle)
                     circle.originPoint = originPointF
-                    Log.e("Cords", "Origin cords X and Y: ${circle.originPoint.x} and ${circle.originPoint.y}" )
+                 //Log.e("Cords", "Origin cords X and Y: ${circle.originPoint.x} and ${circle.originPoint.y}" )
                 }
                 MotionEvent.ACTION_UP -> {
-                    var currentPointF = PointF(event.x, event.y)
+                    val currentPointF = PointF(event.x, event.y)
                     circle.currentPoint = currentPointF
-                    Log.e("Cords", "Current cords X and Y: ${circle.currentPoint.x} and ${circle.currentPoint.y}" )
+                    //Log.e("Cords", "Current cords X and Y: ${circle.currentPoint.x} and ${circle.currentPoint.y}" )
                     invalidate()
                 }
             }
@@ -41,10 +39,8 @@ class DrawViewKT @JvmOverloads constructor(context: Context, attributeSet: Attri
     }
 
     override fun onDraw(canvas: Canvas?) {
-
         for(circle in circles){
             val radius = Math.sqrt(Math.pow(circle.originPoint.x - circle.currentPoint.x.toDouble(), 2.0) + Math.pow(circle.originPoint.y - circle.currentPoint.y.toDouble(), 2.0))
-            Log.e("DRAW", "radius = ${radius}")
             canvas?.drawCircle(circle.originPoint.x, circle.originPoint.y, radius.toFloat(), paint)
        }
         super.onDraw(canvas)
